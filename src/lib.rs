2f67b8c50710400ea7797f11d8dc18f20cf034c8
Prokop <prokop.schield@protonmail.com>
2025-06-10 14:08:48 +0200

    refactor: use constant for byte length

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+pub const BYTES: usize = 16;
+
 /// A UUID represented as a 16-byte array
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct UUID {
-    bytes: [u8; 16],
+    bytes: [u8; BYTES],
 }
